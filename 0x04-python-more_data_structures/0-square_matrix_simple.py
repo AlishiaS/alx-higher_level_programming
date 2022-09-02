3d6ae13e95bb7f3a6a7b75af5c36e13e1aaa1129
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-09-01 23:25:16 -0700

    Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,6 +3,6 @@ def square_matrix_simple(matrix=[]):
     sqrd_matrix = []
 
     for n in range(len(matrix)):
-        sqrd_matrix.append(n**2)
+        sqrd_matrix.append(n**2 for n in matrix)
 
     return sqrd_matrix
