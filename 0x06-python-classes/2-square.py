0c89fe7237002ba9123f881cd3d5802b69588a24
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-09-13 12:49:02 -0700

    My first square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""class Square with size attribute"""
+
+class Square:
+"""Square with size"""
+    def __init__(self, size=0):
+    """Square size"""
+        self.__size = size
---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Class Square with size instance attribute"""
+
+class Square:
+    """Square - class"""
+
+    def __init__(self, size=0):
+    """Square - size"""
+        
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__sizeize = size
+
+        def area(self):
+        """Square - Area"""
+            return (self.__size **2)
