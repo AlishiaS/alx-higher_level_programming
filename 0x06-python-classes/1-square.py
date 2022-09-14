66d369d8e7f98a485fe6f86aca2a0aa00156c0b9
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-09-14 12:26:31 -0700

    Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """Class Square with size instance attribute"""
 
+
 class Square:
     """Square class"""
     def __init__(self, size):
-    """Square size"""
+        """Square size"""
         self.__size = size
