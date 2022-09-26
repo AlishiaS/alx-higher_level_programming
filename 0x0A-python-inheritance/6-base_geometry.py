23d282ca1cb6e737e23b8b4105f445724377bf10
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-09-26 12:33:48 -0700

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Base geometry class"""
+
+    def area(self):
+        """Public instance method"""
+        raise Exception("area() is not implemented")
+
