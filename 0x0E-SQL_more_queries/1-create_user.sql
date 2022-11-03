e396ee41e1b5d5fe048dade8c05ae447f25105a6
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-11-03 21:11:17 +0200

    Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Creates the MySQL server user user_0d_1.
-CREATE IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1-pwd';
-GRANT ALL PRIVILEGES ON * . * TO  user_0d_1@localhost;
+CREATE IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1-pwd';
+GRANT ALL PRIVILEGES ON * . * TO  'user_0d_1'@'localhost';
