d5056bb47bbc08bf05f0ab8d898d29cdd83a6b83
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-11-08 14:48:53 +0200

    Switch

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def delete_at(my_list=[], idx=0):
-    sizeoflist = len(my_list)
-    if idx > sizeoflist - 1 or idx < 0 or my_list is None or idx is None:
-        return (my_list)
+a = 89
+b = 10
+a, b = b, a
+print("a={:d} - b={:d}".format(a, b))
