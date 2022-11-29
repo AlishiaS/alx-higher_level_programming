ed00ebd11e9a0a06b6a9ed060025016ccfedd90e
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-11-29 20:01:21 +0200

    Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,7 +7,7 @@ class SquareP extends SquareP {
 			c = 'X';
 		}
 		for (let i = 0; i < this.height; i++) {
-			let s = '2.';
+			let s = '';
 			for (let j = 0; j < this.width; j++) {
 				s += c;
 			}
