6e6b95bb649e69d4ec1390304bd20b5f070f5ea7
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-11-30 18:32:04 +0200

    Occurrences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
-const Square_ = require('./5-square');
-module.exports = class Square extends Square_ {
-  charPrint (c) {
-    if (c === undefined) {
-      super.print();
-    } else {
-      let i = 0;
-      for (i = 0; i < this.height; i++) {
-        console.log(c.repeat(this.width));
-      }
+// Count the number of occurrences in a list
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      ++count;
     }
   }
+  return count;
 };
