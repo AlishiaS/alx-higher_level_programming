ac26a4545bebde50824d261dfcbe75830daad7b3
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-11-30 18:33:31 +0200

    Occurrences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// Count the number of occurrences in a list
+// returns the number of occurrences in a list
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
   for (let i = 0; i < list.length; i++) {
