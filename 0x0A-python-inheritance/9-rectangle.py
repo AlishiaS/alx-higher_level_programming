6d6efb8039eab601a8002298f11cae2cb3350a71
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-11-30 22:18:00 +0200

    Full rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-"""Defines Rectangle class that inherit from BaseGeometry"""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry 
+""" Provides a class to represent rectangles"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle class that inherits from BaseGeometry"""
+    """ Definition of fixed-size rectangle
+    """
     def __init__(self, width, height):
-        """Initialize a new Rectangle with BaseGeometry"""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        """ Instantiate a rectangle
+        """
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
         self.__width = width
         self.__height = height
 
+    def __str__(self):
+        """ Render a string representation of a rectangle
+        """
+        return '[Rectangle] {}/{}'.format(self.__width, self.__height)
 
-def area(self):
-    """Returns area of Rectangle"""
-    return self.__width * self.__height
-
-
-def __str__(self):
-    """Returns the width and height of Rectangle"""
-    return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+    def area(self):
+        """ Calculate the area of a rectangle
+        """
+        return self.__width * self.__height
