cb4c01c7fa6fbc7b75dfd6e03d76043461deec93
Alishia Subramoney <subramoneyalishia@gmail.com>
2023-01-27 12:09:43 +0200

    Commit file

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,26 +1,19 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+    """Handles basic arithmetic operations."""
     from calculator_1 import add, sub, mul, div
     import sys
-    if len(sys.argv) != 4:
-        print("{}".format("Usage: ./100-my_calculator.py <a> <operator> <b>"))
+
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
+
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    op = sys.argv[2]
-    result = None
-    message = ""
-    if op == "+":
-        result = add(a, b)
-    elif op == "-":
-        result = sub(a, b)
-    elif op == "/":
-        result = div(a, b)
-    elif op == "*":
-        result = mul(a, b)
-    else:
-        message = "Unknown operator. Available operators: +, -, * and /"
-    if result is None:
-        print("{}".format(message))
-        sys.exit(1)
-    print("{:d} {} {:d} = {:d}".format(a, op, b, result))
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
