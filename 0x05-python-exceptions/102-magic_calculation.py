37c743f96183de6f0ccd7d55d3b633c2083de1a8
Alishia Subramoney <subramoneyalishia@gmail.com>
2023-02-07 21:53:59 +0200

    Commit file

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-
-
 def magic_calculation(a, b):
+    """Python bytecode
+    Args:
+        a: integer
+        b: integer
+    Returns: 0
+    """
     result = 0
-
     for i in range(1, 3):
         try:
             if i > a:
-                raise Exception("Too far")
+                raise Exception('Too far')
             else:
                 result += (a ** b) / i
-        except Exception:
-            result = a + b
+        except BaseException:
+            result = b + a
             break
-    return
+    return 
