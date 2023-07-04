7497caac28e1e8d4061aa1adc720db09e5ec29e0
Alishia Subramoney <subramoneyalishia@gmail.com>
2023-07-04 21:31:09 +0200

    Commit file

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
-# ends a request to that URL displays the size of the response body
-curl -sI "$1" | grep 'Content-Length:' | cut -f2 -d' '
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Error: URL argument is missing."
+  exit 1
+fi
+
+response_file=$(mktemp)
+curl -s -o "$response_file" "$1"
+
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to retrieve the response."
+  rm "$response_file"
+  exit 1
+fi
+
+size=$(wc -c < "$response_file")
+
+echo "Size of the response body: $size bytes"
+
+rm "$response_file"
+
